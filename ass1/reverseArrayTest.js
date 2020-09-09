161995e3c092def3251f9231f0bc9ba4e7c096cb
efremKidane <69815763+efremKidane@users.noreply.github.com>
2020-09-09 12:46:15 -0500

    new

--- ass1/reverseArrayTest.js
+++ ass1/reverseArrayTest.js
@@ -4,11 +4,11 @@
 describe(" reverseArray", function() {
 
     it("reverse of a,  b, c is c ,b ,a", function() {
-      assert.equal(  reverseArray(["a", "b", "c"]), (["c", "b", "a"]));
+      assert.deepEqual(  reverseArray(["a", "b", "c"]), (["c", "b", "a"]));
     });
   
     it("reverse of 1, 3, 4 is 4, 3, 1", function() {
-      assert.equal(  reverseArray([1, 3 ,4]), ([4, 3, 1]));
+      assert.deepEqual(  reverseArray([1, 3 ,4]), ([4, 3, 1]));
     });  
   
   });
