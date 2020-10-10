448a71fdcee551d84d2e9c386595f31ca50efa4c
efremKidane <69815763+efremKidane@users.noreply.github.com>
2020-10-09 20:53:51 -0500

    new

---
+++ CS-303/W3D4/examReview.js
@@ -0,0 +1,21 @@
+function printHour(end){
+
+    setTimeout( function stopWatch(){
+       let date = new Date();
+       let hour = date.getHours();
+       let min = date.getMinutes();
+       let sec = date.getSeconds();
+   
+       console.log(hour + ":" + min +":"+ sec);
+       if( end > 0){
+           setTimeout(stopWatch, 1000);
+       }
+       end = end -1;
+   } ,1000);
+   }
+   
+   
+     
+   
+   
+     printHour(10);
